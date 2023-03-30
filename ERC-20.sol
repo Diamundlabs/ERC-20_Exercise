0fee42631eaca35148d0db4b5fcea9755930268e
thexovc <osazeepeter79@gmail.com>
2023-03-30 08:47:53 +0100

    adding comments on task to complete an EIP-20 standard

--- ERC-20.sol
+++ ERC-20.sol
@@ -9,6 +9,17 @@ pragma solidity ^0.8.0;
 
 
 contract ERC20 {
- 
+    //    EIP-20 Standard has the following functions and events
+    //     FUNCTIONS
+    //      *   totalSupply()
+    //      *   balanceOf(account)
+    //      *   transfer(recipient, amount)
+    //      *   allowance(owner, spender)
+    //      *   approve(spender, amount)
+    //      *   transferFrom(sender, recipient, amount)
+    
+    //     EVENTS
+    //      *   Transfer(from, to, value)
+    //      *   Approval(owner, spender, value)
 
 }
