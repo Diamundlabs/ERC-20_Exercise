fcb5490e9278a6c11a79cc43e002451fe6ffe04b
etim <etimpaul22@gmail.com>
2023-04-02 15:44:13 +0100

    added functions for checking balance, transfer tokens and comments for easy reading

--- contracts/diamundToken.sol
+++ contracts/diamundToken.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.0;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
+
+    function findBalance(address owner) external view returns (uint256);
+
+    function transferToken(
+        address _receiver,
+        uint value
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
 contract Exercise_ERC20 is IERC20 {
@@ -24,5 +33,26 @@ contract Exercise_ERC20 is IERC20 {
     // Get total supply of tokens
     function _totalSupply() public view returns (uint256 total) {
         total = totalSupply;
+    }
+
+    // Find DMDT Balance of an address
+    function findBalance(
+        address owner
+    ) public view returns (uint256 balanceOwner) {
+        balanceOwner = balance[owner];
+    }
+
+    // Transfer DMDT Tokens from one address to another
+    function transferToken(
+        address _receiver,
+        uint value
+    ) public returns (bool) {
+        require(balance[msg.sender] >= value, "Insufficient Funds");
+        balance[msg.sender] = balance[msg.sender] - value;
+        balance[_receiver] = balance[_receiver] + value;
+
+        emit Transfer(msg.sender, _receiver, value);
+
+        return true;
     }
 }
